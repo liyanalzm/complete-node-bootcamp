a616a8178dbae88c0e1e0340d3ad8c25fa9da2c2
Nur Liyana Mohd Lazim <liyana@goquo.com>
2020-01-06 15:33:52 +0800

    Add prettier settings(Lesson 22)

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,7 @@
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
 
 const replaceTemplate = require('./modules/replaceTemplate');
 
@@ -21,22 +22,33 @@ const replaceTemplate = require('./modules/replaceTemplate');
 ///////////////////////////////////////////////////////////
 // SERVER
 
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const server = http.createServer((req, res) => {
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
+const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
-  const {query, pathname} = parsedUrl;
+  const { query, pathname } = parsedUrl;
 
   // Overview page
-  if (pathname === "/" || pathname === "/overview") {
+  if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, {
-      "Content-type": "text/html"
+      'Content-type': 'text/html'
     });
 
     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
@@ -44,32 +56,32 @@ const server = http.createServer((req, res) => {
 
     res.end(output);
 
-  // Product page
-  } else if (pathname === "/product") {
+    // Product page
+  } else if (pathname === '/product') {
     res.writeHead(200, {
-      "Content-type": "text/html"
+      'Content-type': 'text/html'
     });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
     res.end(output);
-  
-  // API
-  } else if (pathname === "/api") {
+
+    // API
+  } else if (pathname === '/api') {
     res.writeHead(200, {
-      "Content-type": "application/json"
+      'Content-type': 'application/json'
     });
     res.end(data);
 
-  // Not found
+    // Not found
   } else {
     res.writeHead(404, {
-      "Content-type": "text/html",
-      "my-own-header": "hello-world"
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
     });
-    res.end("<h1>PAGE NOT FOUND</h1>");
+    res.end('<h1>PAGE NOT FOUND</h1>');
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening to requests on port 8000");
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
 });
